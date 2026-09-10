154375317167ee97bce9a0d8e6003c3a77a2678b
selmabasibuyuk-eng <selmabasibuyuk@gmail.com>
2026-09-10 16:38:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,19 @@ load_dotenv()
 KIBANA_BASE_URL = os.getenv("KIBANA_BASE_URL", "https://kibana.srv.team")
 KIBANA_API_KEY = os.getenv("KIBANA_API_KEY")
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
-ERROR_THRESHOLD = float(os.getenv("ERROR_THRESHOLD_PERCENT", "20.0"))
 
-# Virgülle ayrılmış contract slug listesini ayrıştırır
+# Ekibinizdeki herkes buraya kendi partner slug'ını ekleyebilir
 CONTRACT_SLUGS = [
-    s.strip() for s in os.getenv("CONTRACT_SLUGS", "").split(",") if s.strip()
+    "43605.b2b.5f2b",
+    "43605.b2b.ad97",
+    "43605.b2b.554f",
+    "43605.b2b.a79e",
+    "43605.b2b.3626"
 ]
+
+THRESHOLDS = {
+    "HTTP_400_MAX_PERCENT": 20.0,
+    "HTTP_429_MAX_PERCENT": 15.0,
+    "PREBOOK_ERROR_MAX_PERCENT": 20.0,
+    "SEARCH_GROWTH_PERCENT": 10.0
+}
